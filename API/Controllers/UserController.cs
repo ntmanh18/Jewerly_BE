94d4ea6c3f1aaca98ef8e46ee529a7ded1d4b919
ntmanh18 <141035032+ntmanh18@users.noreply.github.com>
2024-06-01 22:53:18 +0700

    User - Update role, Deactive, View, Change password

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -3,6 +3,7 @@ using Data.Model.UserModel;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace API.Controllers
 {
@@ -27,12 +28,49 @@ namespace API.Controllers
         }
 
         [Route("update-user")]
-        [HttpPost]
+        [HttpPut]
 
         public async Task<IActionResult> UpdateUser([FromBody] UpdateUserReqModel userModel)
         {
             string token = Request.Headers["Authorization"].ToString().Split(" ")[1];
             var res = await _userService.UpdateUser(token,userModel);
+            return StatusCode(res.Code, res);
+        }
+
+        [HttpPut]
+        [Route("active-deactive-user")]
+        public async Task<IActionResult> DeactiveUser([FromQuery(Name = "id")] string userId)
+        {
+            string token = Request.Headers["Authorization"].ToString().Split(" ")[1];
+            var res = await _userService.DeactiveUser(token,new DeactiveUserReqModel { UserId = userId });
+            return StatusCode(res.Code, res);
+        }
+
+        [HttpPut]
+        [Route("update-role")]
+        public async Task<IActionResult> UpdateROle([FromBody] UpdateRoleReqModel model)
+        {
+            string token = Request.Headers["Authorization"].ToString().Split(" ")[1];
+            var res = await _userService.UpdateRole(token,model);
+            return StatusCode(res.Code, res);
+        }
+
+        [HttpGet]
+        [Route("view-list-users")]
+
+        public async Task<IActionResult> ViewUserList([FromQuery] UserQueryObject query)
+        {
+            string token = Request.Headers["Authorization"].ToString().Split(" ")[1];
+            var res  = await _userService.ViewUserList(token,query);
+            return StatusCode(res.Code, res);
+        }
+
+        [HttpPut]
+        [Route("change-password")]
+        public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordModel model)
+        {
+            string token = Request.Headers["Authorization"].ToString().Split(" ")[1];
+            var res = await _userService.ChangePassword(token,model);
             return StatusCode(res.Code, res);
         }
     }
---
+++ Data/Model/UserModel/ViewUserResModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Model.UserModel
+{
+    public class ViewUserResModel
+    {
+        public string UserId { get; set; } = null!;
+
+        public string Username { get; set; } = null!;
+
+        public string Password { get; set; } = null!;
+
+        public int Role { get; set; }
+
+        public string FullName { get; set; } = null!;
+
+        public DateOnly DoB { get; set; }
+
+        public string Phone { get; set; } = null!;
+
+        public string Address { get; set; } = null!;
+
+        public bool Status { get; set; }
+    }
+}

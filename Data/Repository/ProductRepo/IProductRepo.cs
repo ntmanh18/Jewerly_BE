1f16430406e66f81a929593db943e6c2ad2d2e44
ntmanh18 <141035032+ntmanh18@users.noreply.github.com>
2024-06-08 18:35:31 +0700

    Test

--- Data/Repository/ProductRepo/IProductRepo.cs
+++ Data/Repository/ProductRepo/IProductRepo.cs
@@ -8,10 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Data.Repository.ProductRepo
-    public interface IProductRepo
- 
-    
-{   
+{ 
     public interface IProductRepo : IRepository<Product>    
     {
         public Task<IEnumerable<Product>> GetProducts();

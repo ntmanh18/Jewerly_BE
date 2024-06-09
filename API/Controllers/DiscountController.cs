5d72e66782276a82d9d35a0ef3c8abb592309dd2
ntmanh18 <141035032+ntmanh18@users.noreply.github.com>
2024-06-09 20:24:49 +0700

    Create Discount , View Discount

---
+++ API/Controllers/DiscountController.cs
@@ -0,0 +1,49 @@
+﻿using Azure.Core;
+using Bussiness.Services.DiscountService;
+using Bussiness.Services.UserService;
+using Data.Model.DiscountModel;
+using Data.Model.UserModel;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class DiscountController : Controller
+    {
+        private readonly IDiscountService _discountService;
+        public DiscountController(IDiscountService discountService)
+        {
+            _discountService = discountService;
+        }
+        [Route("view-discount")]
+        [HttpGet]
+        public async Task<IActionResult> ViewDiscount([FromQuery] DiscountQueryModel query)
+        {
+            string token = Request.Headers["Authorization"].ToString().Split(" ")[1];
+            var res = await _discountService.GetAllDiscount(token, query);
+            return StatusCode(res.Code, res);
+        }
+    
+
+        [Route("create-discount")]
+        [HttpPost]
+        public async Task<IActionResult> CreateDiscount(CreateDiscountReqModel req)
+        {
+            string token = Request.Headers["Authorization"].ToString().Split(" ")[1];
+            var res = await _discountService.CreateDiscount(token, req);
+            return StatusCode(res.Code, res);
+        }
+
+
+        [Route("create-discount-product")]
+        [HttpPost]
+        public async Task<IActionResult> CreateDiscountProduct(CreateDiscountProductReqModel req) { 
+            string token = Request.Headers["Authorization"].ToString().Split(" ")[1];
+            var res = await _discountService.CreateDiscountProduct(token, req);
+            return StatusCode(res.Code, res);
+        }
+    }
+}
---
+++ Data/Repository/DiscountRepo/DiscountRepo.cs
@@ -0,0 +1,44 @@
+﻿using Data.Entities;
+using Data.Repository.GenericRepo;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data.Repository.DiscountRepo
+{
+    public class DiscountRepo : Repository<Discount>, IDiscountRepo
+    {
+        private readonly JewerlyV6Context _context;
+        public DiscountRepo(JewerlyV6Context context) :base(context)
+        {
+            _context = context;
+        }
+        public Task<List<Discount>> GetActiveDiscount(DateTime expiredDate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<Discount>> GetAllDiscount()
+        {
+            return await _context.Discounts.Include(c => c.ProductProducts).Include(c=> c.CreatedByNavigation).Select( c=> new Discount
+            {
+               DiscountId = c.DiscountId,
+               CreatedBy = c.CreatedByNavigation.UserId,
+               ExpiredDay = c.ExpiredDay,
+               PublishDay = c.PublishDay,
+               Cost = c.Cost,
+               CreatedByNavigation = c.CreatedByNavigation,
+               ProductProducts = c.ProductProducts
+
+            }).AsQueryable().ToListAsync();
+        }
+
+        public async Task<Discount> GetDiscountById(string id)
+        {
+            return await _context.Discounts.Include(c => c.ProductProducts).FirstOrDefaultAsync(c => c.DiscountId ==id);
+            }
+    }
+}

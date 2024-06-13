80554f74472326c164265fb2dd7b9ef2b8cac2a1
Mhin28 <minhvb123456@gmail.com>
2024-06-13 10:48:59 +0700

    Fix search voucher

--- Data/Model/VoucherModel/DatePart.cs
+++ Data/Model/VoucherModel/DatePart.cs
@@ -8,6 +8,7 @@ namespace Data.Model.VoucherModel
 {
     public class DatePart
     {
+
         public int Year { get; set; }
         public int Month { get; set; }
         public int Day { get; set; }

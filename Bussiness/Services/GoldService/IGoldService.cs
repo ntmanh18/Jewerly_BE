5cfa06bf2f7b72e2905571237b08fc3e021e671f
trungdq3305 <trungdqse183305@fpt.edu.vn>
2024-06-19 22:14:53 +0700

    Gold CRU

---
+++ Bussiness/Services/GoldService/IGoldService.cs
@@ -0,0 +1,20 @@
+﻿using Data.Model.CustomerModel;
+using Data.Model.GoldModel;
+using Data.Model.ResultModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussiness.Services.GoldService
+{
+    public interface IGoldService
+    {
+        public Task<ResultModel> GetGolds(string token);
+        Task<ResultModel> GetGoldById(string? token, string id);
+        public Task<ResultModel> CreateGold(string token, GoldCreateModel goldModel, string userId);
+        Task<ResultModel> UpdateGold(string? token, GoldUpdateModel goldModel, string userId);
+
+    }
+}

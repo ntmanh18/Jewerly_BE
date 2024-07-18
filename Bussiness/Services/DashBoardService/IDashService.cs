bd59fbdaa6ca13cc09f3c66ad65cc314f5708d36
Mhin28 <96234654+Mhin28@users.noreply.github.com>
2024-07-18 16:38:14 +0700

    up api for dashboard

---
+++ Bussiness/Services/DashBoardService/IDashService.cs
@@ -0,0 +1,16 @@
+﻿using Data.Model.ResultModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bussiness.Services.DashBoardService
+{
+    public interface IDashService
+    {
+        Task<ResultModel> GetTotalIncomeAsync(string? token);
+        Task<ResultModel> GetIncomeByCashNumberAsync(string? token);
+        Task<ResultModel> GetIncomeByMonthAsync(string? token);
+    }
+}
